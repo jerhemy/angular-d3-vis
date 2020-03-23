a246112002c513e4ea7f78f91a53db1466c559b9
Jerhemy Waldon <jswaldon@gmail.com>
2020-03-22 22:51:48 -0700

    updated demo page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,6 +9,62 @@ export class AppComponent implements OnInit {
   title = 'angular-charts';
   chartData: Array<any>;
   marketData: Array<any>;
+  stateData: Array<any>;
+
+  stateOrders = [
+    { state: 'Alabama', value: 0 },
+    { state: 'Alaska', value: 0 },
+    { state: 'Arkansas', value: 0 },
+    { state: 'Arizona', value: 0 },
+    { state: 'California', value: 0 },
+    { state: 'Colorado', value: 0 },
+    { state: 'Connecticut', value: 0 },
+    { state: 'Delaware', value: 0 },
+    { state: 'Florida', value: 0 },
+    { state: 'Georgia', value: 0 },
+    { state: 'Hawaii', value: 0 },
+    { state: 'Iowa', value: 0 },
+    { state: 'Idaho', value: 0 },
+    { state: 'Illinois', value: 0 },
+    { state: 'Indiana', value: 0 },
+    { state: 'Kansas', value: 0 },
+    { state: 'Kentucky', value: 0 },
+    { state: 'Louisiana', value: 0 },
+    { state: 'Maine', value: 0 },
+    { state: 'Maryland', value: 0 },
+    { state: 'Massachusetts', value: 0 },
+    { state: 'Michigan', value: 0 },
+    { state: 'Minnesota', value: 0 },
+    { state: 'Missouri', value: 0 },
+    { state: 'Mississippi', value: 0 },
+    { state: 'Montana', value: 0 },
+    { state: 'North Carolina', value: 0 },
+    { state: 'North Dakota', value: 0 },
+    { state: 'Nebraska', value: 0 },
+    { state: 'New Hampshire', value: 0 },
+    { state: 'New Jersey', value: 0 },
+    { state: 'New Mexico', value: 0 },
+    { state: 'Nebraska', value: 0 },
+    { state: 'Nevada', value: 0 },
+    { state: 'New York', value: 0 },
+    { state: 'Ohio', value: 0 },
+    { state: 'Oklahoma', value: 0 },
+    { state: 'Oregon', value: 0 },
+    { state: 'Pennsylvania', value: 0 },
+    { state: 'Rhode Island', value: 0 },
+    { state: 'South Carolina', value: 0 },
+    { state: 'South Dakota', value: 0 },
+    { state: 'Tennessee', value: 0 },
+    { state: 'Texas', value: 0 },
+    { state: 'Utah', value: 0 },
+    { state: 'Virginia', value: 0 },
+    { state: 'Vermont', value: 0 },
+    { state: 'Washington', value: 0 },
+    { state: 'Wisconsin', value: 0 },
+    { state: 'West Virginia', value: 0 },
+    { state: 'Wyoming', value: 0 },
+    { state: 'Puerto Rico', value: 0 }
+  ];
 
   constructor() {}
 
@@ -39,6 +95,15 @@ export class AppComponent implements OnInit {
       this.marketData.push({
           date: d.setDate(d.getDate() + i),
           value: Math.floor(Math.random() * 1000)
+        }
+      );
+    }
+
+    this.stateData = [];
+    for (let i = 0; i < this.stateOrders.length; i++) {
+      this.stateData.push({
+          state: this.stateOrders[i].state,
+          visited: Math.floor(Math.random() * 255)
         }
       );
     }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BarChartComponent } from './components/bar-chart/bar-chart.component';
 import { LineChartComponent } from './components/line-chart/line-chart.component';
+import { UsMapComponent } from './components/us-map/us-map.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     BarChartComponent,
-    LineChartComponent
+    LineChartComponent,
+    UsMapComponent
   ],
   imports: [
     BrowserModule,
